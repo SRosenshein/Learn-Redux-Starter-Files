1b1eda3f7a0c69a021a856e0615614f4092ff75a
Sam Rosenshein <srosenshein1@gmail.com>
2016-12-15 13:58:49 -0500

    part12

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,5 +1,15 @@
-export default function posts(state = [], action) {
-	console.log("The post will change.");
-	console.log(state, action);
-	return state;
+function posts(state = [], action) {
+	switch (action.type) {
+		case 'INCREMENT_LIKES':
+			// return the updated state
+			console.log("like increment");
+			const i = action.index;
+			return [
+				...state.slice(0,i), {...state[i], likes: state[i].likes + 1}, ...state.slice(i+1)
+			]
+		default:
+			return state;
+	}
 }
+
+export default posts;
